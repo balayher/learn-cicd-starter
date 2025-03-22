53669ebb01bb176304457e63e3783e686ff88422
balayher <balayher@gmail.com>
2025-03-22 13:38:20 -0700

    adding cd

--- json.go
+++ json.go
@@ -32,6 +32,6 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.WriteHeader(code)
 	_, err = w.Write(dat)
 	if err != nil {
-		log.Printf("Error writing data: %s", err)
+		log.Printf("Error writing response: %s", err)
 	}
 }
